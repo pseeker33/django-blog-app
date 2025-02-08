4b5e0c48dc6c2e3f6f022abb2da05a49dd5ffd3b
pseeker33 <zavalah222@gmail.com>
2025-02-08 10:53:11 -0500

    Improving home template styling

--- blog/blogapp/models.py
+++ blog/blogapp/models.py
@@ -5,6 +5,7 @@ class Entry(models.Model):
     entry_title = models.CharField(max_length=50)
     entry_text = models.TextField()
     entry_date = models.DateTimeField(auto_now_add=True)
+    entry_image = models.ImageField(upload_to='blog_images/', blank=True, null=True)  # Campo para la imagen
     entry_autor = models.ForeignKey(User, on_delete=models.CASCADE)
 
     class Meta:
